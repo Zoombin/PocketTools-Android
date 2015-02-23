be0810c0afb81946affb58905213884e9f165c09
daiye <daiye@daiye-PC>
2015-02-23 23:07:51 +0800

    尺子重写

--- src/com/juhe/pockettools/ruler/RulerMainActivity.java
+++ src/com/juhe/pockettools/ruler/RulerMainActivity.java
@@ -1,46 +1,39 @@
 package com.juhe.pockettools.ruler;
 
-import org.xmlpull.v1.XmlPullParser;
-
+import android.app.Activity;
+import android.graphics.Paint;
 import android.os.Bundle;
-import android.util.AttributeSet;
-import android.util.Xml;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.widget.FrameLayout;
 
 import com.juhe.pockettools.R;
-import com.juhe.pockettools.home.FullscreenActivity;
-//import com.fotoable.helpr.wallpaper.w;
 
-public class RulerMainActivity extends FullscreenActivity {
-	private static final String TAG = "Ruler";
-	private RulerViewInch rulerviewinch;
-	private RulerViewMm rulerviewmm;
-	private Button btn_swith;
-	private Button btn_close;
-	private FrameLayout ly_rulers;
+public class RulerMainActivity extends Activity {
 
-	public void onCreate(Bundle bundle) {
-		super.onCreate(bundle);
+	Button btn_close;
+	static Paint paint = new Paint(), textPaint = new Paint();
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_ruler_mian);
-		ly_rulers = ((FrameLayout) findViewById(R.id.ly_rulers));
-//		((ImageView) findViewById(R.id.img_bg)).setImageBitmap(w.a().d());
-		rulerviewinch = new RulerViewInch(this);
-		rulerviewinch.setVisibility(View.INVISIBLE);
-		rulerviewmm = new RulerViewMm(this);
-		ly_rulers.addView(rulerviewinch, 0);
-		ly_rulers.addView(rulerviewmm, 1);
-		btn_swith = ((Button) findViewById(R.id.btn_swith));
-		btn_close = ((Button) findViewById(R.id.btn_close));
-//		btn_swith.setOnClickListener(new a(this));
+		btn_close = (Button) findViewById(R.id.btn_close);
 		btn_close.setOnClickListener(new OnClickListener() {
 			
 			@Override
-			public void onClick(View v) {
+			public void onClick(View arg0) {
 				finish();
 			}
 		});
+
+		paint.setStyle(Paint.Style.STROKE);
+		paint.setStrokeWidth(0);
+		paint.setAntiAlias(false);
+		paint.setColor(0xff000000);
+
+		textPaint.setStyle(Paint.Style.STROKE);
+		textPaint.setStrokeWidth(0);
+		textPaint.setAntiAlias(true);
+		textPaint.setColor(0xff000000);
 	}
 }
--- src/com/juhe/pockettools/ruler/RulerViewMm.java
+++ src/com/juhe/pockettools/ruler/RulerViewMm.java
@@ -2,206 +2,55 @@ package com.juhe.pockettools.ruler;
 
 import android.content.Context;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.view.View;
 
 public class RulerViewMm extends View {
-
-	public static Paint mmpaint = new Paint();
-	public static Paint textpaint = new Paint();
-	public static Paint cmpaint = new Paint();
-	private static final String TAG = "RulerViewMm";
+	static private float pxmm;
+	float width, height;
 	float density = 1.0F;
 	float xdpi = 1.0F;
 	float ydpi = 1.0F;
-	float width = 1.0F;
-	float height = 1.0F;
-	int yperheight = 0;
-	int xperheight = 0;
-	int density_30 = 1;
-	int density_18 = 1;
-	int density_10 = 1;
+//	int yperheight = 0;
+//	int xperheight = 0;
 	private double inch_heightPixels = 1.0D;
-	private double inch_widthPixels = 1.0D;
-
-	public RulerViewMm(Context context) {
-		super(context);
-		initView(context);
-	}
-
-	public RulerViewMm(Context context, AttributeSet attributeset) {
-		super(context, attributeset);
-		initView(context);
-	}
-
-	private void initView(Context paramContext) {
-		DisplayMetrics metrics = paramContext.getResources()
+//	private double inch_widthPixels = 1.0D;
+	
+	public RulerViewMm(Context context, AttributeSet foo) {
+		super(context, foo);
+		DisplayMetrics metrics = context.getResources()
 				.getDisplayMetrics();
 		density = metrics.density;
 		ydpi = metrics.ydpi;
 		xdpi = metrics.xdpi;
 		inch_heightPixels = (metrics.heightPixels / (25.399999999999999D * (metrics.heightPixels / ydpi)));
-		inch_widthPixels = (metrics.widthPixels / (25.399999999999999D * (metrics.widthPixels / xdpi)));
-		density_10 = ((int) (10.0F * density));
-		density_18 = ((int) (18.0F * density));
-		density_30 = ((int) (30.0F * density));
-		mmpaint.setStyle(Paint.Style.STROKE);
-		mmpaint.setStrokeWidth(0.0F);
-		mmpaint.setAntiAlias(false);
-		mmpaint.setColor(Color.WHITE);
-		cmpaint.setStyle(Paint.Style.STROKE);
-		cmpaint.setStrokeWidth(0.0F);
-		cmpaint.setAntiAlias(false);
-		cmpaint.setColor(Color.argb(100, 0, 0, 0));
-		cmpaint.setAlpha(0);
-		textpaint.setStyle(Paint.Style.STROKE);
-		textpaint.setStrokeWidth(0.0F);
-		textpaint.setAntiAlias(true);
-		textpaint.setColor(Color.WHITE);
-		textpaint.setTextSize(density * 16.0F);
+//		inch_widthPixels = (metrics.widthPixels / (25.399999999999999D * (metrics.widthPixels / xdpi)));
+		
+		pxmm = (float)inch_heightPixels;
 	}
 
-//	private void a(Canvas paramCanvas)
-//  {
-//    int i1 = 1;
-//    paramCanvas.save();
-//    int i2 = i1;
-//    float f1 = (float)(i2 * inch_widthPixels);
-//    int i3;
-//    float f2;
-//    int i4;
-//    float f3;
-//    float f4;
-//    if (f1 > width)
-//    {
-//      i3 = i1;
-//      f2 = (float)(i3 * inch_heightPixels);
-//      if (f2 > height)
-//      {
-//        i4 = i1;
-//        f3 = (float)(i4 * inch_heightPixels);
-//        if (f3 <= height) {
-//          break label248;
-//        }
-//        f4 = (float)(i1 * inch_widthPixels);
-//        if (f4 <= width) {
-//          break label331;
-//        }
-//        paramCanvas.restore();
-//      }
-//    }
-//    else
-//    {
-//      if (i2 % 5 == 0) {
-//        m.setColor(Color.argb(50, 255, 255, 255));
-//      }
-//      for (;;)
-//      {
-//        paramCanvas.drawLine(f1, 0.0F, f1, height, m);
-//        i2++;
-//        break;
-//        m.setColor(Color.argb(15, 255, 255, 255));
-//      }
-//    }
-//    if (i3 % 5 == 0) {
-//      m.setColor(Color.argb(50, 255, 255, 255));
-//    }
-//    for (;;)
-//    {
-//      paramCanvas.drawLine(0.0F, f2, width, f2, m);
-//      i3++;
-//      break;
-//      m.setColor(Color.argb(15, 255, 255, 255));
-//    }
-//    label248:
-//    int i5;
-//    if (i4 % 10 == 0) {
-//      i5 = density_30;
-//    }
-//    for (;;)
-//    {
-//      paramCanvas.drawLine(width - i5, f3, width, f3, k);
-//      paramCanvas.drawLine(0.0F, f3, i5, f3, k);
-//      i4++;
-//      break;
-//      if (i4 % 5 == 0) {
-//        i5 = density_18;
-//      } else {
-//        i5 = density_10;
-//      }
-//    }
-//    label331:
-//    int i6;
-//    if (i1 % 10 == 0) {
-//      i6 = density_30;
-//    }
-//    for (;;)
-//    {
-//      paramCanvas.drawLine(f4, 0.0F, f4, i6, k);
-//      i1++;
-//      break;
-//      if (i1 % 5 == 0) {
-//        i6 = density_18;
-//      } else {
-//        i6 = density_10;
-//      }
-//    }
-//  }
-	
-	private void drawRightText(Canvas canvas) {
-		canvas.save();
-		canvas.translate(width - density_30, 0.0F);
-		canvas.rotate(90.0F);
-		for (int i = 1; i <= yperheight; i++) {
-			canvas.drawText(i + "",
-					(float) (10.0D * (i * inch_heightPixels) - textpaint
-							.getTextSize() / 4.0F), textpaint.getTextSize(),
-					textpaint);
-		}
-		canvas.restore();
-	}
-	
-	
-	private void drawLeftText(Canvas canvas) {
-		canvas.save();
-		canvas.translate(density_30, 0.0F);
-		canvas.rotate(90.0F);
-		for (int i = 1; i <= yperheight; i++) {
-			canvas.drawText(i + "",
-					(float) (10.0D * (i * inch_heightPixels) - textpaint
-							.getTextSize() / 4.0F),
-					-textpaint.getTextSize() / 4.0F, textpaint);
-		}
-		canvas.restore();
-	}
-
-	private void drawTopText(Canvas canvas) {
-		canvas.save();
-		for (int i = 1; i <= xperheight; i++) {
-			canvas.drawText(i + "",
-					(float) (10.0D * (i * inch_widthPixels) - textpaint
-							.getTextSize() / 4.0F),
-							density_30 + textpaint.getTextSize(), textpaint);
-		}
-		canvas.restore();
-	}
-	
-	@Override
-	public void onDraw(Canvas canvas) {
-//		drawGraduation(canvas);
-		drawRightText(canvas);
-		drawLeftText(canvas);
-		drawTopText(canvas);
-	}
-
-	@Override
-	protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+	public void onSizeChanged(int w, int h, int oldW, int oldH) {
 		width = w;
 		height = h;
-		yperheight = ((int) Math.round(2.54D * (height / ydpi)));
-		xperheight = ((int) Math.round(2.54D * (width / xdpi)));
+//		yperheight = ((int) Math.round(2.54D * (height / ydpi)));
+//		xperheight = ((int) Math.round(2.54D * (width / xdpi)));
+	}
+
+	public void onDraw(Canvas canvas) {
+		canvas.drawColor(0xffffffff);
+		for (int i = 1;; ++i) {
+			float y = i * pxmm;
+			if (y > height) {
+				break;
+			}
+			int size = (i % 10 == 0) ? 33 : (i % 5 == 0) ? 17 : 10;
+			canvas.drawLine(width - size, y, width, y, RulerMainActivity.paint);
+		}
+		canvas.rotate(90);
+		for (int i = 1; i <= 20; ++i) {
+			canvas.drawText("" + i, i * pxmm * 10 - 10, -17,
+					RulerMainActivity.textPaint);
+		}
 	}
 }
