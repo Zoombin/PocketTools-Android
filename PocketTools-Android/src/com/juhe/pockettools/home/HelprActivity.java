04e156e5a8815ed5fb27e7af98bd408208f8c8a9
daiye830505 <59082342@qq.com>
2015-03-21 12:38:41 +0800

    首页改为天气

--- PocketTools-Android/src/com/juhe/pockettools/home/HelprActivity.java
+++ PocketTools-Android/src/com/juhe/pockettools/home/HelprActivity.java
@@ -1,26 +1,62 @@
 package com.juhe.pockettools.home;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
 import java.util.List;
+
+import org.json.JSONObject;
 
 import android.annotation.SuppressLint;
 import android.os.Bundle;
 import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.GridView;
+import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
-import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.baidu.location.BDLocation;
+import com.baidu.location.BDLocationListener;
+import com.baidu.location.GeofenceClient;
+import com.google.gson.Gson;
+import com.juhe.pockettools.HelprApplication;
 import com.juhe.pockettools.R;
+import com.juhe.pockettools.commonview.HorizontalListView;
+import com.juhe.pockettools.pm.PMEntity;
+import com.juhe.pockettools.pm.PMTools;
 import com.juhe.pockettools.utils.HelprCommUtil;
+import com.juhe.pockettools.weather.Hour;
+import com.juhe.pockettools.weather.Weather;
+import com.juhe.pockettools.weather.WeatherHourAdapter;
+import com.juhe.pockettools.weather.WeatherHourInfo;
+import com.juhe.pockettools.weather.WeatherTools;
+import com.thinkland.sdk.android.DataCallBack;
+import com.thinkland.sdk.android.JuheData;
+import com.thinkland.sdk.android.Parameters;
 
 @SuppressLint("NewApi")
 public class HelprActivity extends FullscreenActivity implements
 		android.widget.RadioGroup.OnCheckedChangeListener {
 
 	View view;
+	private Button btn_setting;
+	private TextView tv_city;
+	private TextView tv_updatetime;
+	private ImageView image_weather;
+	private TextView txt_weather;
+	private HorizontalListView weather_hour_listview;
+	private TextView txt_weather_centigrade;
+	private TextView txt_weather_up;
+	private TextView txt_weather_dn;
+	private TextView txt_pm_value;
+	private TextView txt_pm_state;
 	private GridView gd_modules;
 	private ModulesAdapter adapter;
 	List<ModuleInfo> infos;
@@ -31,7 +67,23 @@ public class HelprActivity extends FullscreenActivity implements
 	private RadioButton btn_tool;
 	private RadioButton btn_life;
 	private RadioButton btn_read;
+	private String cityname = "苏州";
+	private MyLocationListener mMyLocationListener;
+	
+	/**
+	 * 实现实位回调监听
+	 */
+	public class MyLocationListener implements BDLocationListener {
 
+		@Override
+		public void onReceiveLocation(BDLocation location) {
+			if (location != null) {
+				cityname = location.getCity().replace("市", "");
+				getData();
+			}
+		}
+	}
+	
 	protected void onCreate(Bundle bundle) {
 		super.onCreate(bundle);
 		view = View.inflate(this, R.layout.activity_helpr_activity, null);
@@ -41,8 +93,33 @@ public class HelprActivity extends FullscreenActivity implements
 		}
 		setContentView(view);
 
+		mMyLocationListener = new MyLocationListener();
+		HelprApplication.getInstance().mLocationClient.registerLocationListener(mMyLocationListener);
+		HelprApplication.getInstance().mGeofenceClient = new GeofenceClient(getApplicationContext());
+		HelprApplication.getInstance().mLocationClient.start();
+		
+		btn_setting = (Button) findViewById(R.id.btn_setting);
+		btn_setting.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+//				Intent intent = new Intent(HelprActivity.this, SettingActivity.class);
+//				startActivity(intent);
+			}
+		});
+		tv_city = (TextView) findViewById(R.id.tv_city);
+		tv_updatetime = (TextView) findViewById(R.id.tv_updatetime);
+		image_weather = (ImageView) findViewById(R.id.image_weather);
+		txt_weather = (TextView) findViewById(R.id.txt_weather);
+		weather_hour_listview = (HorizontalListView) findViewById(R.id.weather_hour_listview);
+		txt_weather_centigrade = (TextView) findViewById(R.id.txt_weather_centigrade);
+		txt_weather_up = (TextView) findViewById(R.id.txt_weather_up);
+		txt_weather_dn = (TextView) findViewById(R.id.txt_weather_dn);
+		txt_pm_value = (TextView) findViewById(R.id.txt_pm_value);
+		txt_pm_state = (TextView) findViewById(R.id.txt_pm_state);
+		
 		initModuleData();
-
+		
 		radioGroup = (RadioGroup) findViewById(R.id.radiogroup);
 		DisplayMetrics displayMetrics = new DisplayMetrics();
 		getWindowManager().getDefaultDisplay()
@@ -64,6 +141,153 @@ public class HelprActivity extends FullscreenActivity implements
 		gd_modules.setAdapter(adapter);
 	}
 
+	private void getData() {
+		Parameters params = new Parameters();
+		params.add("cityname", cityname);
+		params.add("key", "832463dcc76e758433b22244f4568ba1");
+		JuheData.executeWithAPI(39, "http://v.juhe.cn/weather/index",
+				JuheData.GET, params, new DataCallBack() {
+
+					@Override
+					public void resultLoaded(int err, String reason,
+							String result) {
+
+						if (err == 0) {
+							try {
+								Weather entity = new Gson().fromJson(result,
+										Weather.class);
+								if (entity.getError_code() != 0
+										&& entity.getError_code() != 200) {
+									Toast.makeText(getApplicationContext(),
+											entity.getReason(),
+											Toast.LENGTH_SHORT).show();
+									return;
+								}
+								JSONObject future = new JSONObject(result)
+										.getJSONObject("result").getJSONObject(
+												"future");
+
+								List<Weather.Future> list = new ArrayList<Weather.Future>();
+								SimpleDateFormat sdf = new SimpleDateFormat("yyyyMMdd");
+								for (int i = 0; i<7; i++) {
+									Calendar calendar = Calendar.getInstance();
+									calendar.add(Calendar.DAY_OF_YEAR, i);
+									Date date = calendar.getTime();
+									JSONObject day = future
+											.getJSONObject("day_" + sdf.format(date));
+									Weather.Future futureentity = new Gson()
+											.fromJson(day.toString(),
+													Weather.Future.class);
+									list.add(futureentity);
+								}
+								
+								setData(entity.getResult(), list);
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						} else {
+							Toast.makeText(getApplicationContext(), reason,
+									Toast.LENGTH_SHORT).show();
+						}
+					}
+				});
+
+		JuheData.executeWithAPI(39, "http://v.juhe.cn/weather/forecast3h",
+				JuheData.GET, params, new DataCallBack() {
+
+					@Override
+					public void resultLoaded(int err, String reason,
+							String result) {
+
+						if (err == 0) {
+							try {
+								Hour entity = new Gson().fromJson(result,
+										Hour.class);
+
+								if (entity.getError_code() != 0
+										&& entity.getError_code() != 200) {
+									Toast.makeText(getApplicationContext(),
+											entity.getReason(),
+											Toast.LENGTH_SHORT).show();
+									return;
+								}
+								List<Hour.Result> list = entity.getResult();
+								WeatherHourInfo[] infos = new WeatherHourInfo[list.size()];
+								for (int i=0; i<list.size(); i++) {
+									infos[i] = new WeatherHourInfo();
+									infos[i].weather = list.get(i).getWeather();
+									infos[i].hour = WeatherTools.getDateStr(list.get(i).getSfdate());
+									infos[i].temp =  WeatherTools.getTemp(list.get(i).getTemp1());
+								}
+								weather_hour_listview.setAdapter(new WeatherHourAdapter(getApplicationContext(), infos));
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						} else {
+							Toast.makeText(getApplicationContext(), reason,
+									Toast.LENGTH_SHORT).show();
+						}
+					}
+				});
+		
+		Parameters params1 = new Parameters();
+		params1.add("city", cityname);
+		params1.add("key", "e27f776074aaa9df759bf76bded2fc14");
+		JuheData.executeWithAPI(33,
+				"http://web.juhe.cn:8080/environment/air/pm", JuheData.GET,
+				params1, new DataCallBack() {
+
+					@Override
+					public void resultLoaded(int err, String reason,
+							String result) {
+
+						if (err == 0) {
+							try {
+								PMEntity entity = new Gson().fromJson(result,
+										PMEntity.class);
+								if (entity.getError_code() != 0
+										&& entity.getError_code() != 200) {
+									Toast.makeText(getApplicationContext(),
+											entity.getReason(),
+											Toast.LENGTH_SHORT).show();
+									return;
+								}
+								JSONObject r = ((JSONObject) new JSONObject(
+										result).getJSONArray("result").get(0));
+								String pm25 = r.getString("PM2.5");
+
+								txt_pm_value.setText(pm25);
+								
+								PMEntity.Result entity1 = entity.getResult().get(0);
+								txt_pm_state.setText(PMTools.getQualityStr(Integer.parseInt(entity1.getAQI())));
+								txt_pm_state.setBackgroundColor(PMTools.getQualityBackground(Integer.parseInt(entity1.getAQI())));
+							} catch (Exception e) {
+								e.printStackTrace();
+							}
+						} else {
+							Toast.makeText(getApplicationContext(), reason,
+									Toast.LENGTH_SHORT).show();
+						}
+					}
+				});
+	}
+	
+	private void setData(Weather.Result result, List<Weather.Future> list) {
+		Weather.SK sk = result.getSk();
+		Weather.Today today = result.getToday();
+
+		tv_updatetime.setText("更新" + sk.getTime());
+		tv_city.setText(today.getCity());
+		txt_weather_centigrade.setText(sk.getTemp() + "°");
+		String temperature = today.getTemperature();
+		String[] temp = temperature.split("~");
+		txt_weather_up.setText(temp[1].replace("℃", "") + "°");
+		txt_weather_dn.setText(temp[0].replace("℃", "") + "°");
+		txt_weather.setText(today.getWeather());
+		image_weather.setImageBitmap(HelprCommUtil.getImage(WeatherTools
+				.getWeatherIcon(today.getWeather())));
+	}
+	
 	private void initModuleData() {
 		// 日常工具
 		toolinfos = new ArrayList<ModuleInfo>();
